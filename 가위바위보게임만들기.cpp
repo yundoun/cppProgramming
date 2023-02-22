04d72fac8cb7d237bf510f85fb39b210062c41b0
yundoun <dbsehdns12@gmail.com>
2023-02-22 15:37:07 +0900

    초기 커밋.

---
+++ 가위바위보게임만들기.cpp
@@ -0,0 +1,40 @@
+# include <iostream>
+# include <cstring>
+
+using namespace std;
+
+int main() {
+	cout << "가위 바위 보 게임을 합니다. 가위, 바위, 보 중에서 입력하세요" << endl << "exit를 입력하면 게임을 종료합니다." << endl << endl;
+
+	string a;
+	string b;
+	while (true) {
+
+
+		cout << "로미오 >>";
+		cin >> a;
+
+		if (a == "EXIT") {
+			cout << "게임을 종료합니다.";
+			break;
+		}
+
+		cout << "줄리엣 >>";
+		cin >> b;
+
+		if (a == b) {
+			cout << "무승부 입니다." << endl << endl;
+		}
+		else if (a == "가위" && b == "보") {
+			cout << "로미오의 승리 입니다." << endl << endl;
+		}
+		else if (a == "바위" && b == "가위") {
+			cout << "로미오의 승리 입니다." << endl << endl;
+		}
+		else if (a == "보" && b == "바위") {
+			cout << "로미오의 승리 입니다." << endl << endl;
+		}
+		else
+			cout << "줄리엣의 승리 입니다." << endl << endl;
+	}
+}
